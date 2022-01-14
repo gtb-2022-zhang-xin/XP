9bd7bd89cb35ea7f65dc1608fab0af7d260c00e1
章昕 <zhangxzee@163.com>
2022-01-14 22:42:01 +0800

    refactor: extract readTaskLines

---
+++ app/src/main/java/com/tw/cn/cap/gtb/todo/Constants.java
@@ -0,0 +1,1 @@
+package com.tw.cn.cap.gtb.todo;public class Constants{public static final java.lang.String  USER_HOME_PATH= java.lang.System.getProperty("user.home");public static final java.lang.String TODO_CONFIG_ROOT = ".todo";public static final java.lang.String TASK_FILENAME = "tasks";}
